6ffd1e629c27a95851bd429ec796bd510b97e8ad
ayushwcl <class.mbus@outlook.com>
2024-09-07 18:34:56 +0530

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,14 +1,14 @@
-<?php
 $servername = "mydocsvraz.mysql.database.azure.com";
 $username = "thirdstoat2";
 $password = "Server@1";
 $dbname = "document_collaboration";
+$port = 3306; // Default MySQL port
 
-// Create connection
-$conn =new mysqli($servername, $username, $password, $dbname,3306);
+$conn = mysqli_init();
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . mysqli_error($conn));
+mysqli_real_connect($conn, $servername, $username, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL);
+
+if (mysqli_connect_errno()) {
+    die("Connection failed: " . mysqli_connect_error());
 }
-?>
+
