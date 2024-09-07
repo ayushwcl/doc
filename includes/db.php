00f5d2110e7982406282dc6a81bd5553a09e6315
ayushwcl <class.mbus@outlook.com>
2024-09-07 18:22:58 +0530

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,14 +1,14 @@
 <?php
-$servername = "localhost";
+$servername = "mydocsvraz.mysql.database.azure.com";
 $username = "thirdstoat2";
 $password = "Server@1";
 $dbname = "document_collaboration";
 
 // Create connection
-$conn =new mysqli($servername, $username, $password, $dbname);
+$conn =new mysqli($servername, $username, $password, $dbname,3306);
 
 // Check connection
-if ($conn) {
+if ($conn->connect_error) {
     die("Connection failed: " . mysqli_error($conn));
 }
 ?>
