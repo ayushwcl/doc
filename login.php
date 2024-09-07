a66ff3d6c094599cec6547f2c331d7f2ee590810
ayushwcl <class.mbus@outlook.com>
2024-09-07 18:41:25 +0530

    Update login.php

--- login.php
+++ login.php
@@ -9,12 +9,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = sanitize_input($_POST['username']);
     $password = sanitize_input($_POST['password']);
 
-    $sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+    $sql = "SELECT * FROM users WHERE username = ? AND password = ?"
     $stmt = $conn->prepare($sql);
     $stmt->bind_param('ss', $username, $password);
     $stmt->execute();
     $result = $stmt->get_result();
-
+print_r($result);exit;
     if ($result->num_rows == 1) {
         $user = $result->fetch_assoc();
 
